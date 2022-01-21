bf9782a1b3cc335ae1c5d4497f505dc704afdbde
Leszek <leszek@strixspace.com>
2022-01-21 12:09:43 +0100

    First release

---
+++ script.js
@@ -0,0 +1,63 @@
+import "./style.css";
+
+// PARAMETERS
+const TOKEN = ""; // GitHub Token `repo_deployments` authorized
+const REPO = "your-repo"; // e.g. "my-repository"
+const USER_OR_ORG = "your-name"; // e.g. "your-name"
+
+// GLOBAL VARS
+const URL = `https://api.github.com/repos/${USER_OR_ORG}/${REPO}/deployments`;
+const AUTH_HEADER = `token ${TOKEN}`;
+
+// UTILITY FUNCTIONS
+const getAllDeployments = () =>
+  fetch(`${URL}`, { headers: { authorization: AUTH_HEADER } }).then(val => val.json());
+
+const makeDeploymentInactive = id =>
+  fetch(`${URL}/${id}/statuses`, {
+    method: "POST",
+    body: JSON.stringify({ state: "inactive" }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/vnd.github.ant-man-preview+json",
+      authorization: AUTH_HEADER
+    }
+  }).then(() => id);
+
+const deleteDeployment = id =>
+  fetch(`${URL}/${id}`, {
+    method: "DELETE",
+    headers: { authorization: AUTH_HEADER }
+  }).then(() => id);
+
+// MAIN
+getAllDeployments()
+  .catch(console.error)
+  .then(res => {
+    console.log(`${res.length} enviroments found`);
+    return res;
+  })
+  .then(val => val.map(({ id }) => id))
+  .then(ids => Promise.all(ids.map(id => makeDeploymentInactive(id))))
+  .then(res => {
+    console.log(`${res.length} enviroments marked as "inactive"`);
+    return res;
+  })
+  .then(ids => Promise.all(ids.map(id => deleteDeployment(id))))
+  .then(res => {
+    console.log(`${res.length} enviroments deleted`);
+    return res;
+  })
+  .then(finalResult => {
+    const appDiv = document.getElementById("app");
+    appDiv.innerHTML = `
+      <h1>CLEANUP RESULT</h1>
+      <br>
+      Removed enviroments: ${finalResult.length}
+      <br>
+      <br>Ids:<br>
+      ${JSON.stringify(finalResult)}
+      <br><br><br><br><br><br>
+        <p>(Open up the console)</p>
+    `;
+});
